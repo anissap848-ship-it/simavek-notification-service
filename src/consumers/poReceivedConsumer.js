48b8e5bb3c523add6fbdf69785d8f9a4acfa7c6d
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2026-06-23 18:10:29 +0700

    fix: consume received

--- src/consumers/poReceivedConsumer.js
+++ src/consumers/poReceivedConsumer.js
@@ -1,15 +1,15 @@
-const { handleExpiryAlert } = require('../handlers/notificationHandler');
+const { handlePoReceived } = require("../handlers/notificationHandler");
 
 const consumePoReceived = (channel) => {
-  channel.consume('notification.po_received', (msg) => {
+  channel.consume("notification.po_received", (msg) => {
     if (msg !== null) {
       const data = JSON.parse(msg.content.toString());
-      console.log('[po_received] Received:', data);
-      handleExpiryAlert(data);
+      console.log("[po_received] Received:", data);
+      handlePoReceived(data);
       channel.ack(msg);
     }
   });
-  console.log('Listening on queue: notification.po_received');
+  console.log("Listening on queue: notification.po_received");
 };
 
 module.exports = { consumePoReceived };
